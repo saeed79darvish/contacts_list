78d6a0829b4593359ac3c0404962e907440d22c6
saeed79darvish <s79darvish@gmail.com>
2019-09-18 19:17:46 -0700

    deploy2

---
+++ client/build/precache-manifest.4e682e23202c4f196d9729b4b363e143.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74029bdbc4593b678ec78bd93ef5f92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491dee991e26b1fbf283",
+    "url": "/static/js/2.f75f9014.chunk.js"
+  },
+  {
+    "revision": "5f648526cebef0516926",
+    "url": "/static/js/main.f4887b57.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "c1d5e35d71b80d948364c13461fd0441",
+    "url": "/static/media/backgroun_3d.c1d5e35d.jpg"
+  },
+  {
+    "revision": "705000392720dafa8412bd06838e6ea1",
+    "url": "/static/media/demo.70500039.png"
+  },
+  {
+    "revision": "3c633a14643a4d309892eabda83a51d5",
+    "url": "/static/media/demo1.3c633a14.png"
+  }
+]);
