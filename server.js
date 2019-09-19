946b14e32e01d02d969ecd1e7a6bdbc1f85c6058
saeed79darvish <s79darvish@gmail.com>
2019-09-18 18:35:23 -0700

    deploy9

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const connectDB = require("./config/db");
 const path = require("path");
 
 const app = express();
-app.use(express.json({ extended: false }));
 
+// Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
@@ -21,6 +25,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
