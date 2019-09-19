58ea043ed6e33e20e41a4f92e6b00393bfaf98a7
saeed79darvish <s79darvish@gmail.com>
2019-09-18 17:50:01 -0700

    final3

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//serve static assets in production
-if (process.env.Node_ENV === "production") {
-  //Set static folder
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve((__dirname, "client", "build", "index.html")))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
